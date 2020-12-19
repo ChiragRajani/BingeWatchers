a91aabc7a1f47286b3a9b9d358ee72ccaaca16d5
Chirag <chirag9893@gmail.com>
2020-12-19 19:11:16 +0530

    updated

--- app/src/main/java/com/example/bingewatchers/MainActivity.java
+++ app/src/main/java/com/example/bingewatchers/MainActivity.java
@@ -17,6 +17,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -57,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // createAccount(email1,pwd1);
+
               String  email1 = email.getText().toString();
                 String pwd1 = pwd.getText().toString() ;
 
@@ -81,22 +83,30 @@ public class MainActivity extends AppCompatActivity {
                 user.put("Username", email1);
                 user.put("password",pwd1) ;
                 user.put("Genere", namesList);
-                db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Error adding document", e);
-                            }
-                        });
+                createAccount(email1,pwd1,user);
+
             }
         });
+    }
+    void updateUserinDB(Map userinfo){
+        db.collection("Users")
+                .add(userinfo)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        Toast.makeText(MainActivity.this,"Account Created and Values updated",Toast.LENGTH_SHORT) ;
+
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                        Toast.makeText(MainActivity.this,"Account  Not created "+e.getMessage(),Toast.LENGTH_SHORT) ;
+                    }
+                });
     }
     public void onStart() {
         super.onStart();
@@ -104,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 //        updateUI(currentUser);
     }
-    public void createAccount(String email, String password){
+    public void createAccount(String email, String password,Map userInfo){
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -112,12 +122,14 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(MainActivity.this,"Account Created",Toast.LENGTH_SHORT) ;
+                            updateUserinDB(userInfo);
                             FirebaseUser user = mAuth.getCurrentUser();
 //                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.makeText(MainActivity.this, "Authentication failed."+ task.getException().getMessage(),
                                     Toast.LENGTH_SHORT).show();
 //                            updateUI(null);
                         }
