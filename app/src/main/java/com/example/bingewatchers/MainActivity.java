1dea24811d76b32b45c91cb39e1b06b3bf81c478
Chirag <chirag9893@gmail.com>
2020-12-19 14:02:53 +0530

    db connected and genere adder

--- app/src/main/java/com/example/bingewatchers/MainActivity.java
+++ app/src/main/java/com/example/bingewatchers/MainActivity.java
@@ -12,18 +12,32 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.pchmn.materialchips.ChipsInput;
+import com.pchmn.materialchips.model.ChipInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
     private EditText email;
     private EditText pwd;
     private Button btn;
+    ChipsInput chipsInput ;
     private FirebaseAuth mAuth;
     private static final String TAG = "MyActivity";
-
+    FirebaseFirestore db ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +47,54 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.username);
         pwd = findViewById(R.id.password);
         btn = findViewById(R.id.submit);
+        chipsInput = (ChipsInput) findViewById(R.id.chips_input);
         mAuth = FirebaseAuth.getInstance();
-
-        String pwd1 = "password";
-        String email1 = "cjajmera007@gmail.com";
-
+        List<Chip> contactList = new ArrayList<>();
+        chipsInput.setFilterableList(contactList);
+        Chip ch=new Chip() ;
+        db = FirebaseFirestore.getInstance();
+        chipsInput.setFilterableList(ch.getList());
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createAccount(email1,pwd1);
+               // createAccount(email1,pwd1);
+              String  email1 = email.getText().toString();
+                String pwd1 = pwd.getText().toString() ;
 
+                System.out.println("email: "+email1 +"\npassword is "+pwd1+"\n");
+                System.out.println("Selected List is\n"+chipsInput.getSelectedChipList().size());
+                List<Chip> contactsSelected = (List<Chip>) chipsInput.getSelectedChipList();
+
+                System.out.println(contactsSelected.toString());
+//                       for (int i = 0; i <contactsSelected.size(); i++)
+//                     System.out.print(contactsSelected.get(i).getInfo()+"\n");
+
+                List<String> namesList = new ArrayList<String>();
+                for(Chip person : contactsSelected){
+                    namesList.add(person.getLabel());
+                }
+//                for (int i = 0; i <namesList.size(); i++)
+//                    System.out.print(namesList.get(i)+"\n");
+
+
+                Map<String, Object> user = new HashMap<>();
+                user.put("Username", email1);
+                user.put("password",pwd1) ;
+                user.put("Genere", namesList);
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
             }
         });
     }
