5a964cc6cba8b8577e1e68d70f87137f70f8f33c
Chaitanya J. Ajmera <cjajmera007@gmail.com>
2020-12-20 23:19:18 +0530

    calendar added

--- app/src/main/java/com/example/bingewatchers/MainActivity.java
+++ app/src/main/java/com/example/bingewatchers/MainActivity.java
@@ -83,13 +83,13 @@ public class MainActivity extends AppCompatActivity {
                                 Intent i = new Intent(MainActivity.this, DashBoard.class);
                                 startActivity(i);
                             }
-//                            updateUI(user);
+
                         else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed." + task.getException().getMessage(),
                                     Toast.LENGTH_SHORT).show();
-//                            updateUI(null);
+
                         }
                     }
                 });
