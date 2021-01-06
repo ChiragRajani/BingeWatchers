034e561d05d0f5ff4bdfb5a36c0d063eddcac2ba
Chaitanya J. Ajmera <cjajmera007@gmail.com>
2021-01-06 16:31:38 +0530

    Splash screen

--- app/src/main/java/com/example/bingewatchers/SplashScreen.java
+++ app/src/main/java/com/example/bingewatchers/SplashScreen.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreen extends AppCompatActivity {
     private static int SplashOut = 1000;
 
@@ -17,9 +20,20 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-//                startActivity(new Intent(SplashScreen.this, DashBoard.class));
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                // FirebaseUser currentUser = mAuth.getCurrentUser();
+                if (currentUser != null) {
+                    Intent i = new Intent(SplashScreen.this, DashBoard.class);
+                    startActivity(i);
+                    System.out.println(" user logged in" + currentUser.getEmail());
+                } else {
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+                    System.out.println("no user is logged i");
+                }
                 finish();
             }
-        },SplashOut);
+        }, SplashOut);
     }
 }
